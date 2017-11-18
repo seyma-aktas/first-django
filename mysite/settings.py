85e759d1aad5082e5e60e7f4c9f0ea8d5c283efa
seyma-aktas <aaktasseyma@gmail.com>
2017-11-18 16:03:01 +0200

    Site için HTML dosyasını değiştirdim.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&o&%gm^i36=rdk&l6&szlb-(0^%os#ksi4!**yn5xk82_gs-eg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
